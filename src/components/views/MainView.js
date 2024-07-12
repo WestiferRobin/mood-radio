fbef50e2e9fb3e8d77217687da33195ee6322ee8
Wes <w.webb0919@gmail.com>
2024-07-12 03:19:42 -0700

    updated code and notes from backend work in mood-radio-service

--- src/components/views/MainView.js
+++ src/components/views/MainView.js
@@ -3,11 +3,14 @@ import {
   Paper
 } from '@mui/material';
 import NavigationPaper from '../papers/NavigationPaper';
+import useMoodApi from "../../hooks/useMoodApi";
 
 const MainView = () => {
   const backgroundColor = "black";
   const paperColor = "grey";
   const paddingSize = 10;
+
+  const { username } = useMoodApi();
 
   return (
     <div style={{backgroundColor: backgroundColor, height: "93%"}}>
@@ -17,6 +20,7 @@ const MainView = () => {
             </Grid>
             <Grid item xs={7.3} style={{ padding: paddingSize, paddingLeft: paddingSize / 2, paddingRight: paddingSize / 2}}>
               <Paper square={false} style={{height: "100%", backgroundColor: paperColor}}>
+                { username }
               </Paper>
             </Grid>
             <Grid item xs={2.35} style={{padding: paddingSize, paddingLeft: paddingSize / 2}}>
--- src/hooks/useMoodApi.js
+++ src/hooks/useMoodApi.js
@@ -6,10 +6,12 @@ const useMoodApi = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    const testId = "66c220ab-d26a-4cce-9c59-16d7f133a363";
+
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('/user');
+                const response = await axios.get(`/user/${testId}`);
                 const user = response.data;
                 setUsername(user.username);
                 // setData(response.data);
