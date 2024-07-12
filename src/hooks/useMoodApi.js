f008d00c424d741f5c9a9cfb99b87484e2283c7c
Wesley  Webb <wesleywebb@Wesleys-MBP.lan>
2024-07-12 00:31:21 -0700

    added proxy for the backend services

--- src/hooks/useMoodApi.js
+++ src/hooks/useMoodApi.js
@@ -1,1 +1,29 @@
-// TODO: Create microservice and implement hook
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useMoodApi = () => {
+    const [username, setUsername] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('/user');
+                const user = response.data;
+                setUsername(user.username);
+                // setData(response.data);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return { username, loading, error };
+};
+
+export default useMoodApi;
