e4573ef317b9fdc733972165ecae1e5b8cef9385
Wes <w.webb0919@gmail.com>
2024-08-03 18:33:04 -0700

    checking again

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material';
 import MainView from './components/views/MainView';
-import Player from './components/Player';
 import theme from './theme'; // Import custom theme
 import "./App.css";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App" style={{height: "100vh", width: "100vw", backgroundColor: "red"}}>
+      <div className="App" style={{backgroundColor: "red"}}>
         <MainView />
-        <Player />
       </div>
     </ThemeProvider>
   );
--- src/components/views/MainView.js
+++ src/components/views/MainView.js
@@ -1,10 +1,12 @@
 import { 
+  Box,
   Grid, 
   Paper
 } from '@mui/material';
 import NavigationPaper from '../papers/NavigationPaper';
 import useMoodApi from '../../hooks/mood-library-hooks/useMoodApi';
 import useSearchApi from '../../hooks/mood-library-hooks/useSearchApi';
+import Player from '../Player';
 
 const MainView = () => {
   const backgroundColor = "black";
@@ -15,21 +17,24 @@ const MainView = () => {
   const { artists } = useSearchApi();
 
   return (
-    <Grid container style={{height: "94.5%", backgroundColor: backgroundColor}}>
-      {/* <Grid item xs={2.35} style={{ padding: paddingSize, paddingRight: paddingSize / 2, paddingLeft: paddingSize}}>
-        <NavigationPaper paperColor={paperColor} paddingSize={paddingSize} height={"94%"} />
+    <Box>
+      <Grid container style={{backgroundColor: backgroundColor}}>
+        <Grid item xs={2.35} style={{ padding: paddingSize, paddingRight: paddingSize / 2, paddingLeft: paddingSize}}>
+          <NavigationPaper paperColor={paperColor} paddingSize={paddingSize} height={"94%"} />
+        </Grid>
+        <Grid item xs={7.3} style={{ padding: paddingSize, paddingLeft: paddingSize / 2, paddingRight: paddingSize / 2}}>
+          <Paper square={false} style={{height: "100%", backgroundColor: paperColor}}>
+            { user === null ? "Not Found or Loading" : `Hello ${user.username}!` }
+            { artists.map((value) => <div>{value.name}</div>)}
+          </Paper>
+        </Grid>
+        <Grid item xs={2.35} style={{padding: paddingSize, paddingLeft: paddingSize / 2}}>
+          <Paper square={false} style={{height: "100%", backgroundColor: paperColor}}>
+          </Paper>
+        </Grid>
       </Grid>
-      <Grid item xs={7.3} style={{ padding: paddingSize, paddingLeft: paddingSize / 2, paddingRight: paddingSize / 2}}>
-        <Paper square={false} style={{height: "100%", backgroundColor: paperColor}}>
-          { user === null ? "Not Found or Loading" : `Hello ${user.username}!` }
-          { artists.map((value) => <div>{value.name}</div>)}
-        </Paper>
-      </Grid>
-      <Grid item xs={2.35} style={{padding: paddingSize, paddingLeft: paddingSize / 2}}>
-        <Paper square={false} style={{height: "100%", backgroundColor: paperColor}}>
-        </Paper>
-      </Grid> */}
-    </Grid>
+      <Player />
+    </Box>
   );
 };
 
