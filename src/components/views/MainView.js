10cee82e109054c5a12af5bc29b46917bdd087d7
Wes <w.webb0919@gmail.com>
2024-08-04 06:11:41 -0700

    asdf

--- src/components/views/MainView.js
+++ src/components/views/MainView.js
@@ -17,17 +17,8 @@ const MainView = () => {
   const { artists } = useSearchApi();
 
   return (<>
-    <Grid container style={{backgroundColor: backgroundColor, height: "92.5vh"}}>
-      <Grid item xs={2.35} style={{backgroundColor: "blue"}}>
-        asdf
-      </Grid>
-      <Grid item xs={7.3} style={{backgroundColor: "green"}}>
-        fdsa
-      </Grid>
-      <Grid item xs={2.35} style={{backgroundColor: "yellow"}}>
-        asdf
-      </Grid>
-      {/* <Grid item xs={2.35} style={{ padding: paddingSize, paddingRight: paddingSize / 2, paddingLeft: paddingSize}}>
+    <Grid container style={{backgroundColor: backgroundColor}}>
+      <Grid item xs={2.35} style={{ padding: paddingSize, paddingRight: paddingSize / 2, paddingLeft: paddingSize}}>
         <NavigationPaper paperColor={paperColor} paddingSize={paddingSize} height={"94%"} />
       </Grid>
       <Grid item xs={7.3} style={{ padding: paddingSize, paddingLeft: paddingSize / 2, paddingRight: paddingSize / 2}}>
@@ -39,9 +30,11 @@ const MainView = () => {
       <Grid item xs={2.35} style={{padding: paddingSize, paddingLeft: paddingSize / 2}}>
         <Paper square={false} style={{height: "100%", backgroundColor: paperColor}}>
         </Paper>
-      </Grid> */}
+      </Grid>
     </Grid>
-    <Player />
+    <Box>
+      <Player />
+    </Box>
   </>);
 };
 
